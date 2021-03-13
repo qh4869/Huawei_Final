f8b9cbed90d905a378b14ead0e2047b0ccb13361
qh4869 <WDqiuhao@163.com>
2021-03-13 10:24:16 +0800

    动态规划结构

--- CodeCraft-2021/ssp.cpp
+++ CodeCraft-2021/ssp.cpp
@@ -5,11 +5,12 @@ void ssp(cServer &server, cVM &VM, const cRequests &request) {
 *	- 关于del的地方还可以优化
 *	- 双节点的问题
 *	- 背包问题：动态规划，tree分支定界
+*	- 有一些vm是可以放进已经买的服务器中的，可以改成dp
 */
 	vector<pair<string, string>> addList; // <vmID, vmName>
 	vector<pair<string, string>> delList;
 	int addNum = 0;
-	// 动态规划的数据结构
+	vector<vector<vector<int>>> dynaMat; // 不知道内存够不够
 
 	for (int iDay=0; iDay<request.dayNum; iDay++) {
 		// 当天的所有请求预处理
@@ -25,7 +26,8 @@ void ssp(cServer &server, cVM &VM, const cRequests &request) {
 		}
 		addNum = addList.size();
 
-		// 二分+剪枝（简化版动态规划）
+		// 装进已有的服务器 FF
+		
 
 	}
 }
