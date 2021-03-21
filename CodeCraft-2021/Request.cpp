6d51dcfc362960dcd0920de3b28653ae577f4b05
lyzplcx <lyzplcx@163.com>
2021-03-21 11:21:52 +0800

    修改Request.h，添加Request.cpp，计算出add/delete的请求方差

---
+++ CodeCraft-2021/Request.cpp
@@ -0,0 +1,57 @@
+#include "Request.h"
+
+void cRequest::getNumEachDay() {
+	/* Fn: 计算每天的add/delete请求数量, 结果装入numAddEachDay和numDelEachDay
+	*/
+	// 与天数有关的vector初始化
+	numAddEachDay.resize(dayNum);
+	numDelEachDay.resize(dayNum);
+
+	for (int iDay = 0; iDay < dayNum; iDay++) {
+		//初始化每天的add和delete请求数量
+		numAddEachDay[iDay] = 0;
+		numDelEachDay[iDay] = 0;
+
+		for (int iTerm = 0; iTerm < numEachDay[iDay]; iTerm++) {
+			//求和
+			if (info[iDay][iTerm].type) { // add 请求
+				numAddEachDay[iDay]++;
+			}
+			else { // delete 请求
+				numDelEachDay[iDay]++;
+			}
+		}
+	}
+}
+
+void cRequest::getVarAdd() {
+	/* Fn: 求出Add请求的方差，结果装入 varAdd
+	*/
+	varAdd = 0;
+	double meanAdd = 0;
+	for (int iDay = 0; iDay < dayNum; iDay++) {
+		meanAdd += numAddEachDay[iDay];
+	}
+	meanAdd /= dayNum;
+
+	for (int iDay = 0; iDay < dayNum; iDay++) {
+		varAdd += (numAddEachDay[iDay] - meanAdd) * (numAddEachDay[iDay] - meanAdd);
+		varAdd /= (dayNum - 1);
+	}
+}
+
+void cRequest::getVarDel() {
+	/* Fn: 输出delete请求的方差，结果装入 varDel
+	*/
+	varDel = 0;
+	double meanDel = 0;
+	for (int iDay = 0; iDay < dayNum; iDay++) {
+		meanDel += numDelEachDay[iDay];
+	}
+	meanDel /= dayNum;
+
+	for (int iDay = 0; iDay < dayNum; iDay++) {
+		varDel += (numDelEachDay[iDay] - meanDel) * (numDelEachDay[iDay] - meanDel);
+		varDel /= (dayNum - 1);
+	}
+}
--- CodeCraft-2021/Request.h
+++ CodeCraft-2021/Request.h
@@ -13,6 +13,19 @@ struct sRequestItem {
 class cRequests {
 public:
     int dayNum; // 天数
+    double varAdd; //Add请求的方差
+    double varDel; //Delete请求的方差
     vector<int> numEachDay; // 每天的请求数量
     vector<vector<sRequestItem>> info; // 某一天的 某条 请求
+    vector<int> numAddEachDay; // 每天的add请求数量
+    vector<int> numDelEachDay; // 每天的delete请求数量
+
+        //计算每天的add/delete请求数量, 
+    void getNumEachDay();
+
+    //计算Add请求的方差
+    void getVarAdd();
+
+    //计算delete请求的方差
+    void getVarDel();
 };
