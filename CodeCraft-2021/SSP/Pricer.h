43b476396c2c59f68ff8d90ca7b3181be90ca520
qh4869 <WDqiuhao@163.com>
2021-04-21 16:45:58 +0800

    激进的压价方案

--- CodeCraft-2021/SSP/Pricer.h
+++ CodeCraft-2021/SSP/Pricer.h
@@ -58,7 +58,7 @@ public:
 	  2、估计了个成本后，如果对手迅速降价，导致自己的服务器占空比迅速降低（占空比分布不平稳），可能收不回成本，考虑这一点成本因子稍微大一丢丢比较好
 	  3、Plan B: 调参卡不赔钱的成本价出价，完全不管别的因素
 	  4、Plan A: 尽可能准确估价，调价，预测对手，分段成本分摊等各种方法*/
-	double estCostScale = 0.98; 
+	double estCostScale = 0.9; 
 	// 我也不知道为啥：最前期往已购买的服务器里装 成本会高估 - >然后进入前期 已有的服务器越多 估算的成本越低 (这个是应该的)
 	double disCountVeryEarly = 0.8;
 	// 早期硬件成本不调整
@@ -67,8 +67,8 @@ public:
 	double hardTaxMid = 1.2; 
 	// 后期购买服务器的请求涨价，后期的硬件成本不容易收回来，除了中期开始的硬件成本提价，购买服务器的请求还提价
 	double hardTaxLaterBuy = 2.5; 
-	double veryEarly = 0.1; // 非常早期，
-	double early = 0.25; // 前期天数比例
+	double veryEarly = 1; // 非常早期，
+	double early = 0.3; // 前期天数比例
 	double later = 0.75; // 后期天数
 	int arOrder = 5; // AR预测阶数
 	int validDays = 10; // 只用最多前二十天的数据来训练（担心数据不平稳影响结果）
