18f7505a4d77001c25962291f513e44e59af7562
qh4869 <WDqiuhao@163.com>
2021-04-22 19:52:16 +0800

    添加新的训练数据，删除firstfit中无用的函数，防止编译超时

--- CodeCraft-2021/FF_Server.cpp
+++ CodeCraft-2021/FF_Server.cpp
@@ -1,71 +1,7 @@
 ﻿#include "FF_Server.h"
 
-int cFF_Server::firstFitDouble(int reqCPU, int reqRAM) {
-/* Fn: 双节点部署
-*/
-	for (int iSer = 0; iSer<(int)myServerSet.size(); iSer++) {
-		if (myServerSet[iSer].aIdleCPU > reqCPU / 2 && myServerSet[iSer].bIdleCPU > reqCPU / 2 \
-			&& myServerSet[iSer].aIdleRAM > reqRAM / 2 && myServerSet[iSer].bIdleRAM > reqRAM / 2)
-			return iSer;
-	}
-
-	return -1;
-}
-
-std::tuple<int, bool> cFF_Server::firstFitSingle(int reqCPU, int reqRAM) {
-/* Fn: 单节点部署，bool输入为true表示A节点
-*/
-	for (int iSer = 0; iSer<(int)myServerSet.size(); iSer++) {
-		if (myServerSet[iSer].aIdleCPU > reqCPU && myServerSet[iSer].aIdleRAM > reqRAM)
-			return make_tuple(iSer, true);
-		if (myServerSet[iSer].bIdleCPU > reqCPU && myServerSet[iSer].bIdleRAM > reqRAM)
-			return make_tuple(iSer, false);
-	}
-
-	return make_tuple(-1, false);
-}
-
-int cFF_Server::firstFitByIdleOrdDouble(int reqCPU, int reqRAM) {
-	/* Fn: 待优化：不需要每次对全部排序，而是被部署的服务器调整，新买的服务器调整,遗留
-	*/
-	/*优先装空闲总量最少的服务器*/
-	rankMyServerbyIdle(); // 先排序
-	int serID;
-	for (auto it = myServerOrder.begin(); it != myServerOrder.end(); it++) {
-		serID = it->first;
-		if (myServerSet[serID].aIdleCPU > reqCPU / 2 && myServerSet[serID].bIdleCPU > reqCPU / 2 \
-			&& myServerSet[serID].aIdleRAM > reqRAM / 2 && myServerSet[serID].bIdleRAM > reqRAM / 2)
-			return serID;
-	}
-
-	return -1;
-}
-
-std::tuple<int, bool> cFF_Server::firstFitByIdleOrdSingle(int reqCPU, int reqRAM) {
-	/*优先装空闲总量最少的服务器*/
-	rankMyServerbyIdle(); // 先排序
-	int serID;
-	for (auto it = myServerOrder.begin(); it != myServerOrder.end(); it++) {
-		serID = it->first;
-		if (myServerSet[serID].aIdleCPU > reqCPU && myServerSet[serID].aIdleRAM > reqRAM)
-			return make_tuple(serID, true);
-		if (myServerSet[serID].bIdleCPU > reqCPU && myServerSet[serID].bIdleRAM > reqRAM)
-			return make_tuple(serID, false);
-	}
-
-	return make_tuple(-1, false);
-}
-
 
 bool cFF_Server::mycomp(pair<string, int> i, pair<string, int> j) {
-	return i.second < j.second;
-}
-
-bool cFF_Server::mycompID(pair<int, int> i, pair<int, int> j) {
-	return i.second < j.second;
-}
-
-bool cFF_Server::mycompIDDB(pair<int, double> i, pair<int, double> j) {
 	return i.second < j.second;
 }
 
@@ -99,53 +35,4 @@ string cFF_Server::chooseSer(int reqCPU, int reqRAM, bool isDoubleNode) {
 	}
 
 	return "";
-}
-
-void cFF_Server::rankMyServerbyIdle() {
-	/* Fn: 按照空闲资源的顺序对已有的服务器排序，结果装入myServerOrder <vmName, idle resource>
-	*	- 四种资源直接求和得到结果
-	*/
-	myServerOrder.clear();
-	for (int iSer = 0; iSer<(int)myServerSet.size(); iSer++) {
-		myServerOrder.push_back(make_pair(iSer, myServerSet[iSer].aIdleCPU + myServerSet[iSer].aIdleRAM \
-			+ myServerSet[iSer].bIdleCPU + myServerSet[iSer].bIdleRAM));
-	}
-
-	sort(myServerOrder.begin(), myServerOrder.end(), cFF_Server::mycompID);
-}
-
-void cFF_Server::rankMyServerbyOccupied() {
-	/* Fn: 按照占有资源的顺序排序，结果装入 "同上"
-	*/
-	int ocpResource;
-	string serName;
-
-	myServerOrder.clear();
-	for (int iSer = 0; iSer<(int)myServerSet.size(); iSer++) {
-		serName = myServerSet[iSer].serName;
-		ocpResource = info[serName].totalCPU + info[serName].totalRAM \
-			- myServerSet[iSer].aIdleCPU - myServerSet[iSer].aIdleRAM \
-			- myServerSet[iSer].bIdleCPU - myServerSet[iSer].bIdleRAM;
-		myServerOrder.push_back(make_pair(iSer, ocpResource));
-	}
-
-	sort(myServerOrder.begin(), myServerOrder.end(), cFF_Server::mycompID);
-}
-
-void cFF_Server::rankMyserverbyRatio() {
-/* Fn: 按照空闲占比从小到大，结果装入 myServerOrderDB
-*/
-	double ratio;
-	string serName;
-
-	myServerOrderDB.clear();
-	for (int iSer = 0; iSer<(int)myServerSet.size(); iSer++) {
-		serName = myServerSet[iSer].serName;
-		ratio = (myServerSet[iSer].aIdleCPU + myServerSet[iSer].aIdleRAM \
-			+ myServerSet[iSer].bIdleCPU + myServerSet[iSer].bIdleRAM) \
-			/ (info[serName].totalCPU + info[serName].totalRAM);
-		myServerOrderDB.push_back(make_pair(iSer, ratio));
-	}
-
-	sort(myServerOrderDB.begin(), myServerOrderDB.end(), cFF_Server::mycompIDDB);
 }
